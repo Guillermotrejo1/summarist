13ec220cf7692645b74a6653b8541c46dc668fd2
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-04-09 22:17:35 -0500

    first commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import logo from "../assets/logo.png"
+
+
+
+function NavBar() {
+    return (
+      <nav className="h-[80px]">
+        <div className="flex justify-between items-center max-w-[1070px] w-full h-full mx-auto px-6">
+          <div className="flex justify-center items-center flex-grow">
+            <Image src={logo} width={200} height={80} className="max-w-[200px] h-auto object-contain" alt="Logo" />
+          </div>
+          <ul className="flex gap-6">
+            <li className="cursor-pointer text-[#032b41] hover:text-[#2bd97c] ">Login</li>
+            <li className="cursor-not-allowed text-[#032b41]">About</li>
+            <li className="cursor-not-allowed text-[#032b41]">Contact</li>
+            <li className="cursor-not-allowed text-[#032b41]">Help</li>
+          </ul>
+        </div>
+      </nav>
+    );
+}
+
+export default NavBar;
+
+
+  
