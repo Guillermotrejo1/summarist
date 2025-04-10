40a8d5c5a94254c3927e6407e4bae8d4d59231d6
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-04-09 22:52:11 -0500

    added all the component, need to style them next

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image"
 import logo from "../assets/logo.png"
 
-
-
-function NavBar() {
+const NavBar = () => {
     return (
       <nav className="h-[80px]">
         <div className="flex justify-between items-center max-w-[1070px] w-full h-full mx-auto px-6">
           <div className="flex justify-center items-center flex-grow">
-            <Image src={logo} width={200} height={80} className="max-w-[200px] h-auto object-contain" alt="Logo" />
+            <Image src={logo} width={200} height={80} className="max-w-[400px] h-auto object-contain" alt="Logo" />
           </div>
           <ul className="flex gap-6">
             <li className="cursor-pointer text-[#032b41] hover:text-[#2bd97c] ">Login</li>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
+import FeaturesSection from "@/components/FeaturesSection";
+import Footer from "@/components/Footer";
+import Landing from "@/components/Landing";
 import NavBar from "@/components/NavBar";
+import NumbersSection from "@/components/NumbersSection";
+import ReviewsSection from "@/components/ReviewsSection";
 
 
 export default function Home() {
   return (
     <>
     <NavBar/>
+    <Landing />
+    <FeaturesSection />
+    <ReviewsSection />
+    <NumbersSection />
+    <Footer />
     </>
   );
 }
