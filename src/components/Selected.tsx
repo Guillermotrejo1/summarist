c814f06e8665e0d7c185bf2e8ae8635532bd5672
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-05-08 14:13:41 -0500

    base for selected is complete

---
+++ src/components/Selected.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Login from "../assets/login.png";
+import { FaCirclePlay } from "react-icons/fa6";
+
+const Selected = () => {
+  return (
+    <div>
+      <h1 className="text-[22px] font-bold text-[#032b41] mb-4">
+        Selected just for you
+      </h1>
+      <a
+        href=""
+        className="flex bg-[#fbefd6] p-6 mb-6 rounded-[6px] gap-6 w-[calc(66.66%)] h-50"
+      >
+        <div>
+          <div className="flex w-[calc(66.66%)]">
+            <h2 className="text-sm h-[2.5rem] overflow-hidden">
+              How Constant Innovation Creates Radically Successful Businesses
+            </h2>
+            <div className="w-[1px] bg-[#bac8ce] mx-4 h-38"></div>
+            <div>
+              <div className="flex">
+                <Image src={Login} width={200} height={200} alt="" />
+                <div className="flex flex-col ml-4">
+                  <h1 className="font-bold whitespace-nowrap">The Lean Startup</h1>
+                  <h2>Eric Ries</h2>
+                  <div className="flex mt-6">
+                    <FaCirclePlay className="text-4xl"/>
+                    <h3 className="flex ml-3 font-bold text-[14px] text-[#032b41] whitespace-nowrap items-center">3 mins 23 secs</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default Selected;
+
+// width: 100%;
+//     height: 100%;
+//     background-color: #000;
+//     color: #fff;
+//     display: flex
+// ;
+//     justify-content: center;
+//     border-radius: 50%;
+//     align-items: center;
+//     padding: 4px 4px 4px 6px;
--- src/pages/forYou.tsx
+++ src/pages/forYou.tsx
@@ -1,14 +1,20 @@
-import SearchBar from '@/components/SearchBar'
-import Sidebar from '@/components/Sidebar'
-import React from 'react'
+import SearchBar from "@/components/SearchBar";
+import Selected from "@/components/Selected";
+import Sidebar from "@/components/Sidebar";
+import React from "react";
 
 const forYou = () => {
   return (
     <div>
-      <Sidebar/>
-      <SearchBar/>
+      <Sidebar />
+      <SearchBar />
+      <div className="py-10 w-[calc(100%-200px)] ml-50 h-screen">
+        <div className="max-w-[1070px] w-full m-auto px-6">
+          <Selected />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default forYou
+export default forYou;
