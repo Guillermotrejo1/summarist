5721d7f0b2fa9e58c59252b24de3465fbb42be25
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-05-08 12:19:36 -0500

    searchbar complete

--- src/pages/forYou.tsx
+++ src/pages/forYou.tsx
@@ -1,3 +1,4 @@
+import SearchBar from '@/components/SearchBar'
 import Sidebar from '@/components/Sidebar'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const forYou = () => {
   return (
     <div>
       <Sidebar/>
+      <SearchBar/>
     </div>
   )
 }
