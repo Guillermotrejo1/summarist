b7e7fceaffa119de4afc67925d3b854c80d7bf68
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-05-20 22:08:24 -0500

    linked the books, added the correct padding to center the divs and added bg on hover when user hovers over books

--- src/components/Suggested.tsx
+++ src/components/Suggested.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { CiClock2 } from "react-icons/ci";
 import { IoMdStarOutline } from "react-icons/io";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 interface Book {
   id: string;
@@ -49,30 +50,38 @@ const Suggested = () => {
       <h4 className="font-light text-[#394547] mb-4">Browse those books</h4>
       <div className="flex">
         {books.map((book) => (
-          <div key={book.id} className="w-1/5 p-1">
-            <Image src={book.imageLink} width={170} height={100} alt="" />
-            <h2 className="font-bold text-[#032b41] mb-2 line-clamp-2 w-[150px]">
-              Zero to One
-            </h2>
-            <h3 className="text-sm text-[#6b757b] font-light mb-2">
-              Peter Theil with Blake Masters
-            </h3>
-            <h4 className="text-sm text-[#394547] mb-2">
-              Notes on Startups, or How to build The Future
-            </h4>
-            <div className="flex">
+          <Link
+            key={book.id}
+            href={`book/${book.id}`}
+            className="w-1/5 pt-[32px] pr-[12px] pb-[12px] pl-[12px] rounded-sm hover:bg-[#f4f5f5d4] flex items-center justify-center"
+          >
+            <div>
+              <Image src={book.imageLink} width={170} height={100} alt="" />
+              <h2 className="font-bold text-[#032b41] mb-2 line-clamp-2 w-[150px]">
+                Zero to One
+              </h2>
+              <h3 className="text-sm text-[#6b757b] font-light mb-2">
+                Peter Theil with Blake Masters
+              </h3>
+              <h4 className="text-sm text-[#394547] mb-2">
+                Notes on Startups, or How to build The Future
+              </h4>
               <div className="flex">
-                <CiClock2 className="text-lg font-light text-[#6b757b]" />
-                <h4 className="text-sm ml-1 font-light text-[#6b757b]">
-                  03:24
-                </h4>
-              </div>
-              <div className="flex ml-2">
-                <IoMdStarOutline className="text-lg font-light text-[#6b757b]" />
-                <h4 className="text-sm ml-1 font-light text-[#6b757b]">4.3</h4>
+                <div className="flex">
+                  <CiClock2 className="text-lg font-light text-[#6b757b]" />
+                  <h4 className="text-sm ml-1 font-light text-[#6b757b]">
+                    03:24
+                  </h4>
+                </div>
+                <div className="flex ml-2">
+                  <IoMdStarOutline className="text-lg font-light text-[#6b757b]" />
+                  <h4 className="text-sm ml-1 font-light text-[#6b757b]">
+                    4.3
+                  </h4>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
