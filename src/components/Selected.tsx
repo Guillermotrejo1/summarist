348010e61663c219cf9af15170a56076d94511e4
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-06-01 15:13:01 -0500

    reverted changes

--- src/components/Selected.tsx
+++ src/components/Selected.tsx
@@ -41,7 +41,7 @@ const Selected = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div>
       <h1 className="text-[22px] font-bold text-[#032b41] mb-4">
         Selected just for you
       </h1>
--- src/components/Suggested.tsx
+++ src/components/Suggested.tsx
@@ -43,44 +43,38 @@ const Suggested = () => {
   }, []);
 
   return (
-    <div className="py-16 flex flex-col">
+    <div className="py-16">
       <h1 className="text-[22px] font-bold text-[#032b41] mb-2">
         Suggested Books
       </h1>
       <h4 className="font-light text-[#394547] mb-4">Browse those books</h4>
-      <div className="flex justify-center flex-wrap">
+      <div className="flex">
         {books.map((book) => (
-          <Link
-            key={book.id}
-            href={`book/${book.id}`}
-            className="w-1/5 pt-[32px] pr-[12px] pb-[12px] pl-[12px] rounded-sm hover:bg-[#f4f5f5d4] flex items-center justify-center"
-          >
-            <div>
-              <Image src={book.imageLink} width={170} height={100} alt="" />
-              <h2 className="font-bold text-[#032b41] mb-2 line-clamp-2 w-[150px]">
-                Zero to One
-              </h2>
-              <h3 className="text-sm text-[#6b757b] font-light mb-2">
-                Peter Theil with Blake Masters
-              </h3>
-              <h4 className="text-sm text-[#394547] mb-2">
-                Notes on Startups, or How to build The Future
-              </h4>
+          <Link key={book.id} href={`book/${book.id}`} className="w-1/5 pt-[32px] pr-[12px] pb-[12px] pl-[12px] rounded-sm hover:bg-[#f4f5f5d4] flex items-center justify-center">
+          <div>
+            <Image src={book.imageLink} width={170} height={100} alt="" />
+            <h2 className="font-bold text-[#032b41] mb-2 line-clamp-2 w-[150px]">
+              Zero to One
+            </h2>
+            <h3 className="text-sm text-[#6b757b] font-light mb-2">
+              Peter Theil with Blake Masters
+            </h3>
+            <h4 className="text-sm text-[#394547] mb-2">
+              Notes on Startups, or How to build The Future
+            </h4>
+            <div className="flex">
               <div className="flex">
-                <div className="flex">
-                  <CiClock2 className="text-lg font-light text-[#6b757b]" />
-                  <h4 className="text-sm ml-1 font-light text-[#6b757b]">
-                    03:24
-                  </h4>
-                </div>
-                <div className="flex ml-2">
-                  <IoMdStarOutline className="text-lg font-light text-[#6b757b]" />
-                  <h4 className="text-sm ml-1 font-light text-[#6b757b]">
-                    4.3
-                  </h4>
-                </div>
+                <CiClock2 className="text-lg font-light text-[#6b757b]" />
+                <h4 className="text-sm ml-1 font-light text-[#6b757b]">
+                  03:24
+                </h4>
+              </div>
+              <div className="flex ml-2">
+                <IoMdStarOutline className="text-lg font-light text-[#6b757b]" />
+                <h4 className="text-sm ml-1 font-light text-[#6b757b]">4.3</h4>
               </div>
             </div>
+          </div>
           </Link>
         ))}
       </div>
