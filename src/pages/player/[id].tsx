4b21860ce9992505f2d340e0be606b01ef2163a1
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-06-29 10:27:43 -0500

    added a skeleton loading to the audio player

--- src/pages/player/[id].tsx
+++ src/pages/player/[id].tsx
@@ -3,7 +3,9 @@ import Sidebar from "@/components/Sidebar";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "next/navigation";
-import AudioPlayer  from "@/components/player/AudioPlayer";
+import AudioPlayer from "@/components/player/AudioPlayer";
+import { ImSpinner8 } from "react-icons/im";
+import Skeleton from "@mui/material/Skeleton";
 
 interface Book {
   id: string;
@@ -26,6 +28,7 @@ interface Book {
 
 const Id = () => {
   const [bookInfo, setBookInfo] = useState<Book | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(true);
   const params = useParams();
   const id = params?.id;
 
@@ -34,6 +37,7 @@ const Id = () => {
     console.log("Fetching book with ID:", id);
 
     const fetchData = async () => {
+      setIsLoading(true);
       try {
         const response = await axios.get(
           `https://us-central1-summaristt.cloudfunctions.net/getBook?id=${id}`
@@ -42,6 +46,8 @@ const Id = () => {
         setBookInfo(response.data);
       } catch (error) {
         console.error("Error fetching data", error);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchData();
@@ -54,21 +60,38 @@ const Id = () => {
         <Sidebar />
       </div>
       <div className="w-[calc(100%-200px)] ml-[200px] px-6 pt-6">
-        <div className="flex flex-col justify-center items-center max-w-[800px] w-full mx-auto">
-          <div className="text-2xl text-[#032b41] border-b border-[#e1e7ea] mb-8 p-4 w-full">
-            <strong>{bookInfo?.title}</strong>
+        {isLoading ? (
+          <>
+            <div className="flex flex-col items-center justify-center h-screen">
+              <ImSpinner8 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 spinner text-5xl" />
+            </div>
+          </>
+        ) : (
+          <div className="flex flex-col justify-center items-center max-w-[800px] w-full mx-auto">
+            <div className="text-2xl text-[#032b41] border-b border-[#e1e7ea] mb-8 p-4 w-full">
+              <strong>{bookInfo?.title}</strong>
+            </div>
+            <div className="whitespace-pre-line">{bookInfo?.summary}</div>
           </div>
-          <div className="whitespace-pre-line">{bookInfo?.summary}</div>
-        </div>
+        )}
       </div>
-      {/* Pass all necessary bookInfo properties to AudioPlayer */}
-      {bookInfo && (
-        <AudioPlayer
-          audioLink={bookInfo.audioLink}
-          title={bookInfo.title}
-          author={bookInfo.author}
-          imageLink={bookInfo.imageLink} // Pass imageLink here
-        />
+      {isLoading ? (
+        <div className="absolute bottom-2 left-8 right-4 gap-4 flex">
+          <Skeleton variant="rectangular" width={40} height={40} />
+          <div>
+            <Skeleton variant="text" width={200} height={30} />
+            <Skeleton variant="text" width={200} height={30} />
+          </div>
+        </div>
+      ) : (
+        bookInfo && (
+          <AudioPlayer
+            audioLink={bookInfo.audioLink}
+            title={bookInfo.title}
+            author={bookInfo.author}
+            imageLink={bookInfo.imageLink}
+          />
+        )
       )}
     </>
   );
