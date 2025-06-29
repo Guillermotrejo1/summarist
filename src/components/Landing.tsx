05a80db854475e621597937188b3453a8e09ecc2
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-06-29 12:21:00 -0500

    landing page responsive

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -20,23 +20,23 @@ const Landing = () => {
         <div className="row">
           <div className="flex">
             <div className="w-full flex flex-col gap-6">
-              <div className="text-[#032b41] text-[40px] font-bold mb-6">
-                Gain more knowledge <br className="remove--tablet" />
+              <div className="text-[#032b41] text-2xl text-center md:text-left md:text-[40px] font-bold mb-6">
+                Gain more knowledge <br className="hidden md:inline" />
                 in less time
               </div>
-              <div className="text-[#394547] text-xl font-[300px] mb-6 leading-6">
+              <div className="text-[#394547] text-xl font-[300px] md:mb-6 leading-6 text-center md:text-left">
                 Great summaries for busy people,
-                <br className="remove--tablet" />
+                <br className="hidden md:inline"/>
                 individuals who barely have time to read,
-                <br className="remove--tablet" />
+                <br className="hidden md:inline"/>
                 and even people who don’t like to read.
               </div>
-              <button className="btn max-w-[300px] flex items-center" onClick={handleOpenModal}>
+              <button className="btn max-w-[300px] flex items-center mx-auto md:mx-0" onClick={handleOpenModal}>
                 Login
               </button>
               {isModalOpen && <LoginModal onClose={handleCloseModal}/>}
             </div>
-            <figure className="w-full h-full max-w-[400px]">
+            <figure className="hidden w-full h-full max-w-[400px] md:block">
               <Image
                 src={landing}
                 width={200}
