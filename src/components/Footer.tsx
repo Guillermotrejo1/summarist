2f3a370d9a95f2b94e28b891cf520d1dc0d6981c
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-06-29 13:31:48 -0500

    footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,71 +4,71 @@ const Footer = () => {
     <section id="footer">
       <div className="container">
         <div className="row">
-          <div className="relative flex justify-between text-sm">
+          <div className="relative flex flex-col md:flex-row justify-between text-sm gap-4">
             <div className="z-1">
-              <div className="font-semibold mb">Actions</div>
+              <div className="font-semibold mb-2">Actions</div>
               <div>
-                <div className="footer__link--wrapper">
-                  <a className="footer__link">Summarist Magazine</a>
+                <div className="flex items-center gap-4 mb-2">
+                  <a className="flex">Summarist Magazine</a>
                 </div>
-                <div className="footer__link--wrapper">
-                  <a className="footer__link">Cancel Subscription</a>
+                <div className="flex items-center gap-4 mb-2">
+                  <a className="flex">Cancel Subscription</a>
                 </div>
-                <div className="footer__link--wrapper">
-                  <a className="footer__link">Help</a>
+                <div className="flex items-center gap-4 mb-2">
+                  <a className="flex">Help</a>
                 </div>
-                <div className="footer__link--wrapper">
-                  <a className="footer__link">Contact us</a>
+                <div className="flex items-center gap-4 mb-2">
+                  <a className="flex">Contact us</a>
                 </div>
               </div>
             </div>
             <div className="footer__block">
-              <div className="font-semibold mb">Useful Links</div>
+              <div className="font-semibold mb-2">Useful Links</div>
               <div>
-                <div className="footer__link--wrapper">
-                  <a className="footer__link">Pricing</a>
+                <div className="flex items-center gap-4 mb-2">
+                  <a className="flex">Pricing</a>
                 </div>
-                <div className="footer__link--wrapper">
-                  <a className="footer__link">Summarist Business</a>
+                <div className="flex items-center gap-4 mb-2">
+                  <a className="flex">Summarist Business</a>
                 </div>
-                <div className="footer__link--wrapper">
-                  <a className="footer__link">Gift Cards</a>
+                <div className="flex items-center gap-4 mb-2">
+                  <a className="flex">Gift Cards</a>
                 </div>
-                <div className="footer__link--wrapper">
-                  <a className="footer__link">Authors & Publishers</a>
+                <div className="flex items-center gap-4 mb-2">
+                  <a className="flex">Authors & Publishers</a>
                 </div>
               </div>
             </div>
             <div className="footer__block">
-              <div className="font-semibold mb-4 text-lg text-[#032b41] ">Company</div>
+              <div className="font-semibold mb-2 text-lg text-[#032b41] ">Company</div>
               <div>
-                <div className="flex items-center gap-4 mb-4">
+                <div className="flex items-center gap-4 mb-2">
                   <a className="flex">About</a>
                 </div>
-                <div className="flex items-center gap-4 mb-4">
+                <div className="flex items-center gap-4 mb-2">
                   <a className="flex">Careers</a>
                 </div>
-                <div className="flex items-center gap-4 mb-4">
+                <div className="flex items-center gap-4 mb-2">
                   <a className="flex">Partners</a>
                 </div>
-                <div className="flex items-center gap-4 mb-4">
+                <div className="flex items-center gap-4 mb-2">
                   <a className="flex">Code of Conduct</a>
                 </div>
               </div>
             </div>
             <div className="z-1">
-              <div className="font-semibold mb">Other</div>
+              <div className="font-semibold mb-2">Other</div>
               <div>
-                <div className="flex items-center gap-4 mb-4">
+                <div className="flex items-center gap-4 mb-2">
                   <a className="flex">Sitemap</a>
                 </div>
-                <div className="flex items-center gap-4 mb-4">
+                <div className="flex items-center gap-4 mb-2">
                   <a className="flex">Legal Notice</a>
                 </div>
-                <div className="flex items-center gap-4 mb-4">
+                <div className="flex items-center gap-4 mb-2">
                   <a className="flex">Terms of Service</a>
                 </div>
-                <div className="flex items-center gap-4 mb-4">
+                <div className="flex items-center gap-4 mb-2">
                   <a className="flex">Privacy Policies</a>
                 </div>
               </div>
