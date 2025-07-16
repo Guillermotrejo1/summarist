a9b9cdb070855339cfe9abc3e5ff822acd619c13
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-07-16 15:41:38 -0500

    fixed the ui for the signIn modal to show login when signed out and ui for when user wants to register

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -3,6 +3,11 @@ import { useRouter } from "next/router";
 import { BsX } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
 import { IoPerson } from "react-icons/io5";
+import { auth } from "../firebase/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 interface LoginModalProps {
   onClose: () => void;
@@ -11,6 +16,9 @@ interface LoginModalProps {
 const LoginModal: React.FC<LoginModalProps> = ({ onClose }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSignUp, setIsSignUp] = useState(false);
 
   const handleGuestLogin = () => {
     setIsLoading(true);
@@ -19,64 +27,150 @@ const LoginModal: React.FC<LoginModalProps> = ({ onClose }) => {
     }, 1000);
   };
 
+  const register = async () => {
+    setIsLoading(true);
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      console.log("User registered successfully");
+      // Redirect or perform additional actions after registration
+      router.push("/forYou");
+    } catch (error) {
+      console.error("Error registering user:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const login = async () => {
+    setIsLoading(true);
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      //Redirect or perform additional actions after login
+      router.push("/forYou");
+    } catch (error) {
+      console.error("Error logging in user:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-black/40 z-50 flex justify-center items-center">
-      <div className="relative w-[400px] h-[500px] bg-white rounded-lg p-8">
-        <p className="text-center text-[20px] font-bold text-[#032b41] mb-2">
-          Log in to Summarist
+      <div className="relative w-[400px] bg-white rounded-lg p-8">
+        <p className="text-center text-[20px] font-bold text-[#032b41] mb-4">
+          {isSignUp ? "Sign up to Summarist" : "Log in to Summarist"}
         </p>
-        <div className="flex flex-col justify-center items-center">
-          <div className="w-full justify-end">
+        {isSignUp ? (
+          <>
+            <div className="flex flex-col justify-center items-center">
+              <div className="w-full justify-end">
+                <button className="flex btn-google items-center justify-center w-full mb-4 relative">
+                  <FcGoogle className="absolute left-2 text-3xl bg-[#fff] rounded" />
+                  <span>Login with Google</span>
+                </button>
+              </div>
+              <div className="mb-4">
+                <span className="or-text relative text-sm">or</span>
+              </div>
+              <input
+                className="border-2 border-[#bac8ce] w-full p-2 rounded mb-4"
+                type="text"
+                placeholder="Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} // Update email state
+              />
+              <input
+                className="border-2 border-[#bac8ce] w-full p-2 rounded mb-4"
+                type="text"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} //Update password state
+              />
+              <button className="btn w-full flex items-center mb-4" onClick={login}>
+                Login
+              </button>
+            </div>
             <button
-              className="flex btn-guest items-center justify-center w-full mb-4 relative"
-              onClick={handleGuestLogin}
-              disabled={isLoading}
+              className="absolute top-2 right-2 text-5xl"
+              onClick={onClose}
             >
-              {isLoading ? (
-                <div className="flex items-center justify-center">
-                  <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-[#fff]"></div>
-                </div>
-              ) : (
-                <>
-                  <IoPerson className="absolute left-2 text-2xl" />
-                  <span>Login as a Guest</span>
-                </>
-              )}
+              <BsX />
             </button>
-          </div>
-          <div className="mb-4 flex justify-center items-center">
-            <span className="or-text relative text-sm">or</span>
-          </div>
-          <div className="w-full justify-end">
-            <button className="flex btn-google items-center justify-center w-full mb-4 relative">
-              <FcGoogle className="absolute left-2 text-3xl bg-[#fff] rounded" />
-              <span>Login with Google</span>
+            <button
+              className="absolute right-0 rounded-b-lg hover:!bg-[#e0e5e3] text-[#116be9] w-full text-center p-2 !bg-[#f1f6f4]"
+              onClick={register}
+            >
+              Dont have an account?
             </button>
-          </div>
-          <div className="mb-4">
-            <span className="or-text relative text-sm">or</span>
-          </div>
-          <input
-            className="border-2 border-[#bac8ce] w-full p-2 rounded mb-4"
-            type="text"
-            placeholder="Email Address"
-          />
-          <input
-            className="border-2 border-[#bac8ce] w-full p-2 rounded mb-4"
-            type="text"
-            placeholder="Password"
-          />
-          <button className="btn w-full flex items-center">Login</button>
-        </div>
-        <button className="absolute top-2 right-2 text-5xl" onClick={onClose}>
-          <BsX />
-        </button>
-        <div className="text-[#116be9] text-center p-2 mt-4">
-          Forgot Password?
-        </div>
-        <button className="absolute right-0 rounded-b-lg hover:!bg-[#e0e5e3] text-[#116be9] w-full text-center p-2 !bg-[#f1f6f4]">
-          Dont have an account?
-        </button>
+          </>
+        ) : (
+          <>
+            <div className="flex flex-col justify-center items-center">
+              <div className="w-full justify-end">
+                <button
+                  className="flex btn-guest items-center justify-center w-full mb-4 relative"
+                  onClick={handleGuestLogin}
+                  disabled={isLoading}
+                >
+                  {isLoading ? (
+                    <div className="flex items-center justify-center">
+                      <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-[#fff]"></div>
+                    </div>
+                  ) : (
+                    <>
+                      <IoPerson className="absolute left-2 text-2xl" />
+                      <span>Login as a Guest</span>
+                    </>
+                  )}
+                </button>
+              </div>
+              <div className="mb-4 flex justify-center items-center">
+                <span className="or-text relative text-sm">or</span>
+              </div>
+              <div className="w-full justify-end">
+                <button className="flex btn-google items-center justify-center w-full mb-4 relative">
+                  <FcGoogle className="absolute left-2 text-3xl bg-[#fff] rounded" />
+                  <span>Login with Google</span>
+                </button>
+              </div>
+              <div className="mb-4">
+                <span className="or-text relative text-sm">or</span>
+              </div>
+              <input
+                className="border-2 border-[#bac8ce] w-full p-2 rounded mb-4"
+                type="text"
+                placeholder="Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} // Update email state
+              />
+              <input
+                className="border-2 border-[#bac8ce] w-full p-2 rounded mb-4"
+                type="text"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} //Update password state
+              />
+              <button className="btn w-full flex items-center" onClick={login}>
+                Login
+              </button>
+            </div>
+            <button
+              className="absolute top-2 right-2 text-5xl"
+              onClick={onClose}
+            >
+              <BsX />
+            </button>
+            <div className="text-[#116be9] text-center p-2 mt-4">
+              Forgot Password?
+            </div>
+            <button
+              className="absolute right-0 rounded-b-lg hover:!bg-[#e0e5e3] text-[#116be9] w-full text-center p-2 !bg-[#f1f6f4]"
+              onClick={register}
+            >
+              Dont have an account?
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
