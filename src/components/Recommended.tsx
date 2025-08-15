21be9032477547b025cdd124908657cd279a625e
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-08-14 20:59:17 -0500

    moved all the fetching to the forYou instead on the individual components, also renamed the recommended file for consistentsy

---
+++ src/components/Recommended.tsx
@@ -0,0 +1,133 @@
+import Image from "next/image";
+import { CiClock2 } from "react-icons/ci";
+import { IoMdStarOutline } from "react-icons/io";
+import Link from "next/link";
+import Skeleton from "@mui/material/Skeleton";
+
+interface Book {
+  id: string;
+  author: string;
+  title: string;
+  subTitle: string;
+  imageLink: string;
+  audioLink: string;
+  totalRating: number;
+  averageRating: number;
+  keyIdeas: number;
+  type: string;
+  status: string;
+  subscriptionRequired: boolean;
+  summary: string;
+  tags: string[];
+  bookDescription: string;
+  authorDescription: string;
+}
+
+interface Props {
+  books: Book[];
+  isLoading: boolean;
+}
+
+const Recommended = ({ books, isLoading }: Props) => (
+  <>
+    <h1 className="text-[22px] font-bold text-[#032b41] mb-4">
+      Recommended For You
+    </h1>
+    <h4 className="font-light text-[#394547] mb-4">
+      We Think you&apos;ll like this
+    </h4>
+    <div
+      className="flex items-center gap-12 overflow-x-scroll max-w-5xl"
+      style={{
+        scrollbarWidth: "none",
+        msOverflowStyle: "none",
+        WebkitOverflowScrolling: "touch",
+      }}
+    >
+      {isLoading
+        ? new Array(5).fill(0).map((_, index) => (
+            <div className="p-2" key={index}>
+              <Skeleton variant="rectangular" width={170} height={170} />
+              <h2>
+                <Skeleton
+                  variant="text"
+                  width={170}
+                  height={30}
+                  sx={{ borderRadius: "0px" }}
+                />
+              </h2>
+              <h3>
+                <Skeleton
+                  variant="text"
+                  width={170}
+                  height={30}
+                  sx={{ borderRadius: "0px" }}
+                />
+              </h3>
+              <h4>
+                <Skeleton
+                  variant="text"
+                  width={150}
+                  height={50}
+                  sx={{ borderRadius: "0px" }}
+                />
+              </h4>
+              <h4>
+                <Skeleton
+                  variant="text"
+                  width={170}
+                  height={30}
+                  sx={{ borderRadius: "0px" }}
+                />
+              </h4>
+            </div>
+          ))
+        : books.map((book) => (
+            <Link
+              key={book.id}
+              href={`book/${book.id}`}
+              className="pt-[32px] pr-[12px] pb-[12px] pl-[12px] rounded-sm hover:bg-[#f4f5f5d4] flex items-center justify-center relative"
+            >
+              <div>
+                {book.subscriptionRequired && (
+                  <div className="absolute top-1 right-2 bg-[#032b41] text-white text-[10px] font-bold py-1 px-2 rounded-full">
+                    Premium
+                  </div>
+                )}
+                <Image
+                  src={book.imageLink}
+                  width={170}
+                  height={100}
+                  alt={book.title}
+                />
+                <h2 className="font-bold text-[#032b41] mb-2 line-clamp-2 w-[150px]">
+                  {book.title}
+                </h2>
+                <h3 className="text-sm text-[#6b757b] font-light mb-2">
+                  {book.author}
+                </h3>
+                <h4 className="text-sm text-[#394547] mb-2">
+                  {book.subTitle}
+                </h4>
+                <div className="flex">
+                  <div className="flex">
+                    <CiClock2 className="text-lg font-light text-[#6b757b]" />
+                    <h4 className="text-sm ml-1 font-light text-[#6b757b]">
+                      4:24
+                    </h4>
+                  </div>
+                  <div className="flex ml-2">
+                    <IoMdStarOutline className="text-lg font-light text-[#6b757b]" />
+                    <h4 className="text-sm ml-1 font-light text-[#6b757b]">
+                      {book.averageRating}
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+    </div>
+  </>
+);
+
+export default Recommended;
