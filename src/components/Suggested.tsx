ae5e576eb0c976375a59c306da0a9a3969ddaa71
Guillermo Trejo <maynardtrejo@yahoo.com>
2025-09-01 23:16:56 -0500

    removed the book cover from suggested

--- src/components/Suggested.tsx
+++ src/components/Suggested.tsx
@@ -1,5 +1,8 @@
+import Image from "next/image";
+import { CiClock2 } from "react-icons/ci";
+import { IoMdStarOutline } from "react-icons/io";
+import Link from "next/link";
 import Skeleton from "@mui/material/Skeleton";
-import BookCover from "../components/bookCover";
 
 export interface Book {
   id: string;
@@ -77,7 +80,45 @@ const Suggested = ({ books, isLoading }: SuggestedProps) => (
               </h4>
             </div>
           ))
-        : books.map((book) => <BookCover book={book} key={book.id} userData={null} />)}
+        : books.map((book) => (
+            <Link
+              key={book.id}
+              href={`book/${book.id}`}
+              className="pt-[32px] pr-[12px] pb-[12px] pl-[12px] rounded-sm hover:bg-[#f4f5f5d4] flex items-center justify-center relative"
+            >
+              <div>
+                {book.subscriptionRequired && (
+                  <div className="absolute top-1 right-2 bg-[#032b41] text-white text-[10px] font-bold py-1 px-2 rounded-full">
+                    Premium
+                  </div>
+                )}
+                <Image src={book.imageLink} width={170} height={100} alt={book.title} />
+                <h2 className="font-bold text-[#032b41] mb-2 line-clamp-2 w-[150px]">
+                  {book.title}
+                </h2>
+                <h3 className="text-sm text-[#6b757b] font-light mb-2">
+                  {book.author}
+                </h3>
+                <h4 className="text-sm text-[#394547] mb-2">
+                  {book.subTitle}
+                </h4>
+                <div className="flex">
+                  <div className="flex">
+                    <CiClock2 className="text-lg font-light text-[#6b757b]" />
+                    <h4 className="text-sm ml-1 font-light text-[#6b757b]">
+                      03:24
+                    </h4>
+                  </div>
+                  <div className="flex ml-2">
+                    <IoMdStarOutline className="text-lg font-light text-[#6b757b]" />
+                    <h4 className="text-sm ml-1 font-light text-[#6b757b]">
+                      {book.averageRating}
+                    </h4>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
     </div>
   </div>
 );
